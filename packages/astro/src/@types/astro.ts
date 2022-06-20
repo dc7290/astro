fd1c53d2bbecd7d7cf95af5d310ccf3a46da3dc1
Nate Moore <nate@astro.build>
2022-06-20 08:58:04 -0500

    feat: pass all slots to renderers

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -736,7 +736,7 @@ export interface AstroConfig extends z.output<typeof AstroConfigSchema> {
 export type AsyncRendererComponentFn<U> = (
 	Component: any,
 	props: any,
-	children: string | undefined,
+	slots: Record<string, string>,
 	metadata?: AstroComponentMetadata
 ) => Promise<U>;
 
